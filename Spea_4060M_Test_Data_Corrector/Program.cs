3713d297df3f19d1fadcfef1632db6e917247a96
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2022-05-23 11:42:32 -0400

    Add files via upload

---
+++ Spea_4060M_Test_Data_Corrector/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Spea_4060M_Test_Data_Corrector {
+    internal static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Spea_4060M_Test_Data_Corrector());
+        }
+    }
+}
